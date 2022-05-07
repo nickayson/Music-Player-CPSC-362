585937112aeae46ef8c93c80737790fc4d10d907
jre98 <jre98@csu.fullerton.edu>
2022-05-07 03:53:02 +0300

    Finalized help window and more help window

--- help.py
+++ help.py
@@ -36,18 +36,18 @@ class Help:
         self.helpTitle.pack(anchor= 'n')
 
 		# Mute button function info
-        text_="""Mute button information:\nOur mute button will record your volume level from before mute was\nhit; this level will be restored upon being unmuted. The volume can be\nunmuted by interacting with any of the volume control buttons or the slider."""
+        text_="""Mute button information:\nWhen you press the mute button, the volume level will be set to zero.\nThe volume can be unmuted by increasing the volume by interacting with\nthe volume slider or by pressing the volume up button."""
         text_length = len(text_)
         font_size = int((w_h_ratio / text_length) / x)
-        self.muteInfo = tk.Label(self.HelpWindow, bg = 'blue', fg = 'white', font=("Gotham Medium typeface",font_size - 8,"bold"), text=text_) 
+        self.muteInfo = tk.Label(self.HelpWindow, bg = 'blue', fg = 'white', font=("Gotham Medium typeface",font_size - 6,"bold"), text=text_) 
 		# Anchor label to NW corner (top left)
         self.muteInfo.pack(anchor= 'nw')
 
 		# Info about viewing the music library
-        text_="""How to view your Library:\nYour music library comprises all of the songs that you have loaded into\nthe player from your device. You can view this by pressing the library\nbutton from the home screen. You can also sort your songs by different\ncriteria using the sort option(?). You can perform any normal actions\non these songs, such as adding to a playlist or the queue."""
+        text_="""Library Page:\nThe library screen is a way to see all of the songs you have loaded\nfrom your device. The screen will display the name of the folder you have\nselected as well as the music files that are in that folder. You can\nalso click on a song on the list to play it. There is also a load\nsong button on the screen so you can load more songs."""
         text_length = len(text_)
         font_size = int((w_h_ratio / text_length) / x)
-        self.LibraryInfo = tk.Label(self.HelpWindow, bg = 'purple', fg = 'white', font=("Gotham Medium typeface",font_size,"bold"), text=text_)
+        self.LibraryInfo = tk.Label(self.HelpWindow, bg = 'purple', fg = 'white', font=("Gotham Medium typeface",font_size + 2,"bold"), text=text_)
 		# Anchor just below above Label
         self.LibraryInfo.pack(anchor='ne', pady= 10)
 
@@ -55,30 +55,24 @@ class Help:
         text_= """Loading Songs:\nYou can easily add songs from your computer by uploading them as files.\nSimply click on the "Load Song" button to bring up a window from which you\ncan navigate through your files to find songs to upload. Once a song has\nbeen uploaded, it will appear in your library. There are additional 'Load\nSong' buttons on the Queue and Library Pages."""
         text_length = len(text_)
         font_size = int((w_h_ratio / text_length) / x)
-        self.about_loadsong = tk.Label(self.HelpWindow, bg = '#4AC1E1', fg = 'white', font=("Gotham Medium typeface",font_size,"bold"), text= text_)	
+        self.about_loadsong = tk.Label(self.HelpWindow, bg = '#4AC1E1', fg = 'white', font=("Gotham Medium typeface",font_size + 3,"bold"), text= text_)	
 		# Anchor label to West point (middle left)
         self.about_loadsong.pack(anchor= 'w', pady= 1)
 
 		# Information about the Queue
-        text_= """Queue Information:\nYou can view your current queue by pressing the queue button on the\nhomepage. This will bring up a new window that will show what songs\nare next to be played and in what order they will be played."""
+        text_= """Queue Information:\nYou can view your current queue by pressing the queue button on the\nhomepage. This will bring up a new window that will show what songs\nare next to be played and in what order they will be played. You can play\nsongs from this window as well by clicking on them."""
         text_length = len(text_)
         font_size = int((w_h_ratio / text_length) / x)
-        self.queueInfo = tk.Label(self.HelpWindow, bg= 'green', fg = 'white', font=("Gotham Medium typeface",font_size - 8,"bold"), text= text_)
+        self.queueInfo = tk.Label(self.HelpWindow, bg= 'green', fg = 'white', font=("Gotham Medium typeface",font_size - 2,"bold"), text= text_)
 		# Anchor label to E corner (middle right)
         self.queueInfo.pack(anchor= 'e', pady= 10)
 
-		# Label that tells how to make playlists
-        text_ = """Creating a Playlist:\nNot sure if we are implementing this button, so this is just a placeholder for now.\n\n"""
-        text_length = len(text_)
-        font_size = int((w_h_ratio / text_length) / x)
-        self.creatingPlaylists = tk.Label(self.HelpWindow, bg= '#E00DF9', fg = 'white', font=("Gotham Medium typeface",font_size - 33,"bold"), text=text_)
-        # Anchor label to E corner (middle right)
-        self.creatingPlaylists.pack(anchor= 'sw', pady= 1)
-		# Button to open the additional help window
-
-        self.moreHelp = tk.Button(self.HelpWindow, bg='#FF0000', fg= 'white', font=("Gotham Medium typeface", 12, "bold"))
+        # Created a class instance so we can call the openMoreHelp function from this class
+        h = Help()
+        # Button to open the additional help window
+        self.moreHelp = tk.Button(self.HelpWindow, bg='#FF0000', fg= 'white', font=("Gotham Medium typeface", 14, "bold"))
         self.moreHelp['text'] = 'For more help,\nclick here'
-        self.moreHelp['command'] = self.openMoreHelp
+        self.moreHelp['command'] = h.openMoreHelp
         self.moreHelp.pack(anchor='center', pady= 10)
 
 	# More help window where we can have a labeled image of what each button does
@@ -90,5 +84,8 @@ class Help:
         self.moreHelpWindow.geometry("%dx%d" % (width, height))
 
         self.moreHelpWindow['bg'] = 'black'
+        # Puts a label containing an image with more descriptions of what buttons do
+        self.descriptions = tk.Label(self.moreHelpWindow, image=moreHelp)
+        self.descriptions.pack(anchor='center')
 
         
