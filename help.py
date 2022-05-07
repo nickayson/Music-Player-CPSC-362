8e0192096805089bd34e2769e8957da11a8d7154
jre98 <jre98@csu.fullerton.edu>
2022-05-07 05:16:37 +0300

    Font size fixed again

--- help.py
+++ help.py
@@ -39,7 +39,7 @@ class Help:
         text_="""Mute button information:\nWhen you press the mute button, the volume level will be set to zero.\nThe volume can be unmuted by increasing the volume by interacting with\nthe volume slider or by pressing the volume up button."""
         text_length = len(text_)
         font_size = int((w_h_ratio / text_length) / x)
-        self.muteInfo = tk.Label(self.HelpWindow, bg = 'blue', fg = 'white', font=("Gotham Medium typeface",font_size - 8,"bold"), text=text_) 
+        self.muteInfo = tk.Label(self.HelpWindow, bg = 'blue', fg = 'white', font=("Gotham Medium typeface",font_size - 9,"bold"), text=text_) 
 		# Anchor label to NW corner (top left)
         self.muteInfo.pack(anchor= 'nw')
 
@@ -47,15 +47,15 @@ class Help:
         text_="""Library Page:\nThe library screen is a way to see all of the songs you have loaded\nfrom your device. The screen will display the name of the folder you have\nselected as well as the music files that are in that folder. You can\nalso click on a song on the list to play it. There is also a load\nsong button on the screen so you can load more songs."""
         text_length = len(text_)
         font_size = int((w_h_ratio / text_length) / x)
-        self.LibraryInfo = tk.Label(self.HelpWindow, bg = 'purple', fg = 'white', font=("Gotham Medium typeface",font_size + 0,"bold"), text=text_)
+        self.LibraryInfo = tk.Label(self.HelpWindow, bg = 'purple', fg = 'white', font=("Gotham Medium typeface",font_size - 1,"bold"), text=text_)
 		# Anchor just below above Label
-        self.LibraryInfo.pack(anchor='ne', pady= 10)
+        self.LibraryInfo.pack(anchor='ne', pady= 1)
 
 		# Info for how to load songs
         text_= """Loading Songs:\nYou can easily add songs from your computer by uploading them as files.\nSimply click on the "Load Song" button to bring up a window from which you\ncan navigate through your files to find songs to upload. Once a song has\nbeen uploaded, it will appear in your library. There are additional 'Load\nSong' buttons on the Queue and Library Pages."""
         text_length = len(text_)
         font_size = int((w_h_ratio / text_length) / x)
-        self.about_loadsong = tk.Label(self.HelpWindow, bg = '#4AC1E1', fg = 'white', font=("Gotham Medium typeface",font_size + 1,"bold"), text= text_)	
+        self.about_loadsong = tk.Label(self.HelpWindow, bg = '#4AC1E1', fg = 'white', font=("Gotham Medium typeface",font_size,"bold"), text= text_)	
 		# Anchor label to West point (middle left)
         self.about_loadsong.pack(anchor= 'w', pady= 1)
 
@@ -63,7 +63,7 @@ class Help:
         text_= """Queue Information:\nYou can view your current queue by pressing the queue button on the\nhomepage. This will bring up a new window that will show what songs\nare next to be played and in what order they will be played. You can play\nsongs from this window as well by clicking on them."""
         text_length = len(text_)
         font_size = int((w_h_ratio / text_length) / x)
-        self.queueInfo = tk.Label(self.HelpWindow, bg= 'green', fg = 'white', font=("Gotham Medium typeface",font_size - 4,"bold"), text= text_)
+        self.queueInfo = tk.Label(self.HelpWindow, bg= 'green', fg = 'white', font=("Gotham Medium typeface",font_size - 5,"bold"), text= text_)
 		# Anchor label to E corner (middle right)
         self.queueInfo.pack(anchor= 'e', pady= 10)
 
